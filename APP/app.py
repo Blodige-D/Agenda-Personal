80ad852c1e41d9444b637c647a17666627ea1924
Diego Ramos <nicolasrz122@gmail.com>
2023-10-09 11:43:01 -0500

    creacion flask sqlalchemy

--- APP/app.py
+++ APP/app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from models import db, Contact, Telefono, Correo  # Importa los modelos
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mi_agenda.db'
+db.init_app(app)
 
 @app.route("/")
 def index():
@@ -11,6 +16,11 @@ def index():
 def mostrar_contacto(id):
     # Lógica para mostrar un contacto específico
     return f"Información del contacto {id}"
+
+@app.route('/contactos')
+def listar_contactos():
+    contactos = Contact.query.all()
+    return render_template('listar_contactos.html', contactos=contactos)
 
 if __name__ == "__main__":
     app.run(debug=True)
